3360e71070e819a2ea38d29f00790af38961b3fb
Maite DELGADO <maite.delgado42@gmail.com>
2022-05-19 14:10:59 +0200

    setting up

---
+++ src/components/App/index.js
@@ -0,0 +1,20 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+import './styles.scss';
+
+// == Composant
+const App = () => (
+  <div className="app">
+    <header />
+    <main>
+      <div>map</div>
+      <div>flight plans</div>
+    </main>
+    <footer />
+  </div>
+);
+
+// == Export
+export default App;
