83c299ea9a186a6123cab3ebdf98b86dde7e3ddd
Maite DELGADO <maite.delgado42@gmail.com>
2022-05-19 14:45:49 +0200

    footer basic

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,9 +7,16 @@ import PropTypes from 'prop-types';
 // == Imports locaux
 import './styles.scss';
 
-const Footer = ({ prop1 }) => (
-  <footer>Footer</footer>
-);
+const Footer = ({ prop1 }) => {
+  // get the current year, to automatically update
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="footer">
+      <div className="footer--credits">© {year}, Drone fight planner. Tous droits réservés.</div>
+    </footer>
+  );
+};
 
 Footer.propTypes = {
   prop1: PropTypes.string,
